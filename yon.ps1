71dc26231a0ac07767b387d301354d9fe199758f
UV -- KP <98892047+UV-KP@users.noreply.github.com>
2022-02-02 17:45:26 +0330

    Add files via upload

---
+++ yon.ps1
@@ -0,0 +1,13 @@
+﻿Write-Host "The Microsoft Store version of Spotify has been detected which is not supported.`n"
+
+  $ch = Read-Host -Prompt 'Uninstall Spotify Windows Store edition (Y/N)'
+  if ($ch -eq 'y')
+  {
+    Write-Host "Uninstalling Spotify.`n"
+    Get-AppxPackage -Name SpotifyAB.SpotifyMusic | Remove-AppxPackage
+  }
+  else
+  {
+    Read-Host "Exiting...`nPress any key to exit..."
+    exit
+  }
